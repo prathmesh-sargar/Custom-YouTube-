7c013066570ff2e2d3fcbb4aacad9e389632ae91
Prathmesh Sargar <168467615+prathmesh-sargar@users.noreply.github.com>
2024-11-22 23:05:19 +0530

    Formate App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,14 @@ import { BrowserRouter as  Router , Route , Routes} from 'react-router-dom'
 import Watch from "./components/Watch"
 
 function App() {
-  
   return (
     <>
-
   <Router>
-
    <Routes>
-    
       <Route path="/" element={<Homepage/>} />
-      <Route path="/watch" element={<Watch/>} />
-      
+      <Route path="/watch" element={<Watch/>} /> 
    </Routes>
-
-  </Router>
-    
+  </Router>  
     </>
   )
 }
